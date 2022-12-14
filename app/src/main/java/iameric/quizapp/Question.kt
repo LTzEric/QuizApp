c6fe4d84d7f85828f143a2552278ba93671b674a
LTzEric <lichichung.eric@gmail.com>
2022-12-15 00:26:58 +0800

    2022-12-14 Version 1.0.1 ( Build 20221214 )
    -Release Version

--- app/src/main/java/iameric/quizapp/Question.kt
+++ app/src/main/java/iameric/quizapp/Question.kt
@@ -1,3 +1,14 @@
 package iameric.quizapp
 
-data class Question()
+import android.media.Image
+
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
